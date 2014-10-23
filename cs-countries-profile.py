eaac469eadebed06f626a3832a12abb3a2ff5a75
Daniel Silva <daniel.silva@flipside.org>
2014-10-23 16:33:09 +0100

    Add matrix with indicator options.

--- cs-countries-profile.py
+++ cs-countries-profile.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 from datetime import date
 import csv
 import errno
@@ -17,12 +20,37 @@ src_meta_aa = src_dir + 'meta/admin_areas.csv'
 # Language will be replaced before saving the file.
 country_profile_export = export_dir + '{lang}/api/countries-profile/'
 
+# Year to get the data for.
+data_year = 2013
+# How much back should we go to search for data.
+data_year_max_search = data_year - 10
 langs = ['en', 'es']
-indicators = [
-              "NY.GDP.PCAP.KD",  # GDP per capita (constant 2005 US$)
-              "NY.GDP.PCAP.KD.ZG",  # GDP per capita growth (annual %)
-              "SP.POP.TOTL",  # Population, total
-              ]
+indicators = {
+    "NY.GDP.PCAP.KD": {  # GDP per capita (constant 2005 US$)
+        "name": {
+            "en": "GDP per capita",
+            "es": "PIB per cápita"
+        },
+        "unit": "USD",
+        "conversion": "round2"
+    },
+    "NY.GDP.PCAP.KD.ZG": {  # GDP per capita growth (annual %)
+        "name": {
+            "en": "GDP growth per capita",
+            "es": "Crecimiento del PIB per cápita"
+        },
+        "unit": "USD",
+        "conversion": "round2"
+    },
+    "SP.POP.TOTL": {  # Population, total
+        "name": {
+            "en": "Population",
+            "es": "Población"
+        },
+        "unit": "M",
+        "conversion": "million|round2"
+    },  # Population, total
+}
 
 def debug(data):
     """
@@ -110,7 +138,7 @@ def query_worldbank_indicator_api(lang, country, indicator, yr=None):
     else:
         return data[1][0]
 
-def query_worldbank_indicator_api_recursive(lang, country, indicator, preferred_yr, max_yr=2000):
+def query_worldbank_indicator_api_recursive(lang, country, indicator, preferred_yr, max_yr=data_year_max_search):
     """
     Recursively query the API until valid data is found or the max_yr
     is reached.
@@ -172,6 +200,40 @@ def clean_dir(path):
         if exception.errno != errno.EEXIST:
             raise
 
+def apply_conversion(conversion_str, value):
+    """
+    Applies a series of conversions to a value.
+    The value is converted to float so it only makes sense to
+    use this on numeric values.
+    
+    Parameters
+    ----------
+    conversion_str:      : string
+            The conversions to apply separated by |
+    value:               : mixed
+            The value to which the conversions are to be applied
+    """
+    
+    if conversion_str == '' or conversion_str == None:
+        return value
+
+    conversions = conversion_str.split('|');
+    for conv in conversions:
+        if conv == 'round1':
+            value = round(float(value), 1)
+        if conv == 'round2':
+            value = round(float(value), 2)
+        if conv == 'round3':
+            value = round(float(value), 3)
+        if conv == 'round4':
+            value = round(float(value), 4)
+        elif conv == 'million':
+            value = float(value) / 1000000
+        elif conv == 'int':
+            value = int(value)
+
+    return value
+
 def main():
     print "Getting data from Worldbank API"
     # Prepare export directories.
@@ -184,16 +246,12 @@ def main():
         for lang in langs:
             # Init with defaults.
             country_data = { 'name': None, 'iso': iso, 'indicators': [] }
-            for indicator in indicators:
-                ## Get data recursively.
-                data = query_worldbank_indicator_api_recursive(lang, iso, indicator, 2013)
-                indicator_to_append = { 'id': indicator, 'name': None, 'value': None, 'year': None }
+            for indicatorId, indicator in indicators.iteritems():
+                # # Get data recursively.
+                data = query_worldbank_indicator_api_recursive(lang, iso, indicatorId, data_year)
+                indicator_to_append = { 'id': indicatorId, 'name': indicator['name'][lang], 'value': None, 'year': None, 'unit': indicator['unit'], 'exp_year':  data_year}
                 
-                if data == None:
-                    country_data['indicators'].append(indicator_to_append)
-                    continue
-                elif data['value'] == None:
-                    indicator_to_append['name'] = data['indicator']['value']
+                if data == None or data['value'] == None:
                     country_data['indicators'].append(indicator_to_append)
                     continue
                 
@@ -201,8 +259,7 @@ def main():
                 if country_data['name'] == None:
                     country_data['name'] = data['country']['value']
                 
-                indicator_to_append['name'] = data['indicator']['value']
-                indicator_to_append['value'] = data['value']
+                indicator_to_append['value'] = apply_conversion(indicator['conversion'], data['value'])
                 indicator_to_append['year'] = int(data['date'])
                 country_data['indicators'].append(indicator_to_append)
                 
