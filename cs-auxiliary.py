c6779800e9e41c4e26fb99e5def89e24bc1a9cd5
Olaf Veerman <olaf.veerman@gmail.com>
2015-09-04 16:40:51 -0400

    Small refactor of core and auxiliary data processing

--- cs-auxiliary.py
+++ cs-auxiliary.py
@@ -24,204 +24,19 @@ import csv
 import shutil
 import json
 
-
-# Directory structure
-src_dir = 'source/'
-export_dir = 'data/'
-tmp_dir = 'tmp/'
-
-# Source - filenames / dirs
-src_auxiliary = src_dir + 'cs-auxiliary/'
-src_meta_aa = src_dir + 'meta/admin_areas.csv'
-
-# Export filename of the json files
-export_ind = export_dir + '{lang}/api/auxiliary/{indicator}/{aa}.json'
-
-# Languages
-langs = ['en','es']
-# Years we have want data for
-yrs = [2006,2007,2008,2009,2010,2011,2012,2013]
-# The current edition
-current_edition = 2014
-
-
-indicators = [
-  {
-    "id": 107, # The source file contains an indication of the id
-    "custom": True,
-    "export": 'installed-capacity', # Folder for the exported data
-    "title": { # Title of the chart
-      "en": 'Installed capacity',
-      "es": 'Capacidad instalada'
-    },
-    "labelx": { # Label of the x-axis
-      "en": 'year',
-      "es": 'año'
-    },
-    "labely": { # Label of the y-axis
-      "en": 'MW',
-      "es": 'MW'
-    },
-    "series": [
-      {
-        "id": 'non-clean-energy', # The id used in the export
-        "source-id": 'Non-clean Energy', # The id in the source CSV
-        "level": "country", # Data on country level. When 'regional' or 'global' is used, averages are calculated
-        "name": {
-          "en": 'Non-clean Energy',
-          "es": 'Energía no limpia'
-        }
-      },
-      {
-        "id": 'clean-energy', # The id used in the export
-        "source-id": 'Clean Energy', # The id in the source CSV
-        "level": "country",
-        "name": {
-          "en": 'Clean Energy',
-          "es": 'Energía limpia'
-        }
-      }
-    ],
-    "years": yrs # List with years to process
-  },
-  {
-    "id": 201,
-    "export": 'clean-energy-investments',
-    "title": {
-      "en": 'Clean energy investments',
-      "es": 'Inversiones en energías limpias'
-    },
-    "labelx": {
-      "en": 'year',
-      "es": 'año'
-    },
-    "labely": {
-      "en": 'USDm',
-      "es": 'USDm'
-    },
-    "series": [
-      {
-        "id": 'country',
-        "source-id": "Clean energy investments",
-        "level": "country",
-      }
-    ],
-    "years": yrs
-  },
-  {
-    "id": 401,
-    "export": 'carbon-offset-projects',
-    "title": {
-      "en": 'Carbon offset projects by sector',
-      "es": 'Compensaciones de carbono por sector'
-    },
-    "labelx": {
-      "en": 'category',
-      "es": 'categoria'
-    },
-    "labely": {
-      "en": '',
-      "es": ''
-    },
-    "series": [
-      {
-        "id": 'power-generation',
-        "source-id": "Power generation",
-        "level": "country",
-        "name": {
-          "en": 'Power generation',
-          "es": 'Generación eléctrica'
-        }
-      },
-      {
-        "id": 'methane',
-        "source-id": "Methane",
-        "level": "country",
-        "name": {
-          "en": 'Methane',
-          "es": 'Metano'
-        }
-      },
-      {
-        "id": 'forestry',
-        "source-id": "Forestry",
-        "level": "country",
-        "name": {
-          "en": 'Forestry',
-          "es": 'Silvicultura'
-        }
-      },
-      {
-        "id": 'waste',
-        "source-id": "Waste",
-        "level": "country",
-        "name": {
-          "en": 'Waste',
-          "es": 'Residuos'
-        }
-      },
-      {
-        "id": 'energy-efficiency',
-        "source-id": "Energy efficiency",
-        "level": "country",
-        "name": {
-          "en": 'Energy efficiency',
-          "es": 'Eficiencia energética'
-        }
-      },
-      {
-        "id": 'other',
-        "source-id": "Other",
-        "name": {
-          "en": 'Other',
-          "es": 'Otro'
-        }
-      },
-    ],
-    "years": [2014]
-  },
-]
-
-
-def check_dir(d):
-  """Check if a folder (d) exists. If so, ask user to delete it first.
-  """
-  if os.path.exists(d):
-    print 'The directory \'%s\' seems to exist already. Please remove it and run this script again.' % (d)
-    return True
-  else:
-    return False
+import settings
+from utils.utils import check_dir, clean_dir, check_create_folder
 
 
 def get_aa_list():
   """  Returns a list of iso codes for the states and countries from the admin_areas.csv
   """
   aareas = [];
-  ifile = csv.DictReader(open(src_meta_aa))
+  ifile = csv.DictReader(open(settings.src_meta_aa))
   for row in ifile:
     if row["type"] == 'country' or row["type"] == 'state':
       aareas.append(row["iso"])
   return aareas
-
-
-def clean_tmp(full = False):
-  """Clean up the temporary directory.
-
-  Parameters
-  ----------
-  full      : boolean
-              If full is set to True, the tmp directory itself will be deleted
-              as well.
-  """
-  if full:
-    shutil.rmtree(tmp_dir)
-  else:
-    for fn in os.listdir(tmp_dir):
-      file_path = os.path.join(tmp_dir, fn)
-      try:
-        os.unlink(file_path)
-      except Exception, e:
-        print e
 
 
 def main():
@@ -231,20 +46,17 @@ def main():
   #
 
   # Check if tmp folder exists, otherwise create it
-  if check_dir(tmp_dir) == True:
-    sys.exit(0)
-  else:
-    os.makedirs(tmp_dir)
-
+  check_create_folder(settings.tmp_dir)
+  
   # Build the list with countries and states
   admin_areas = get_aa_list()
 
-  for ind in indicators:
-    ind_source = src_auxiliary + str(current_edition) + '-' + str(ind["id"]) + '.csv'
+  for ind in settings.indicators:
+    ind_source = settings.src_auxiliary + str(settings.current_edition) + '-' + str(ind["id"]) + '.csv'
 
     for aa in admin_areas:
       iso = aa.lower()
-      for lang in langs:
+      for lang in settings.langs:
         # Initialize the array that will be written to JSON
         json_data = {"name": iso, "iso": iso, "meta": {"title": ind["title"][lang], "label-x": ind["labelx"][lang], "label-y": ind["labely"][lang]}, "data": []}
 
@@ -274,12 +86,12 @@ def main():
           json_data["data"].append(serie_to_append)
 
         # Write the list to a JSON file
-        file_path = (export_ind).format(lang=lang,indicator=ind["export"],aa=iso)
+        file_path = (settings.exp_aux_json).format(lang=lang,indicator=ind["export"],aa=iso)
         with open(file_path,'w') as ofile:
           json.dump(json_data, ofile)
 
   # Fully remove the temp directory
-  clean_tmp(True)
+  clean_dir(settings.tmp_dir, True)
 
   print "All done. The auxiliary data has been prepared for use on global-climatescope.org."
 
---
+++ utils/utils.py
@@ -0,0 +1,53 @@
+# Climatescope utils
+
+import os.path
+import shutil
+
+
+def check_dir(d):
+  """Check if a folder (d) exists. If so, ask user to delete it first.
+  """
+  if os.path.exists(d):
+    print 'The directory \'%s\' seems to exist already. Please remove it and run this script again.' % (d)
+    return True
+  else:
+    return False
+
+
+def check_create_folder(d):
+  """ Check whether a folder exists, if not the folder is created.
+
+  :param d:
+    Path to the folder
+  :type d:
+    String
+
+  :returns:
+    (String) the path to the folder
+  """
+  if not os.path.exists(d):
+    os.makedirs(d)
+
+
+def clean_dir(d, full = False):
+  """Clean up a directory
+
+  :param d:
+    Path to folder
+  :type d:
+    String
+  :param full:
+    If full is set to True, the directory itself will be deleted
+    as well.
+  :type full:
+    Boolean
+  """
+  if full:
+    shutil.rmtree(d)
+  else:
+    for fn in os.listdir(d):
+      file_path = os.path.join(d, fn)
+      try:
+        os.unlink(file_path)
+      except Exception, e:
+        print e
